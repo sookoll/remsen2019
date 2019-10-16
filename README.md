705fe84430190a4d409c3b2dfc46cac2f021daaa
Mihkel Oviir <mihkel.oviir@gmail.com>
2019-10-17 02:57:12 +0300

    Deploy

--- README.md
+++
@@ -1,2 +0,0 @@
-# Remote Sensing 17.10.2019
-Presentation about KappaZeta web map
--- vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  lintOnSave: 'error',
-  // runtimeCompiler: process.env.NODE_ENV !== 'production'
-}
