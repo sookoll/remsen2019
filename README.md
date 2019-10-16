f7238395d258eebd1f7ed1847b554dacab7f849e
Mihkel Oviir <mihkel.oviir@gmail.com>
2019-10-17 02:50:48 +0300

    presentation

---
+++ README.md
@@ -0,0 +1,2 @@
+# Remote Sensing 17.10.2019
+Presentation about KappaZeta web map
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+#app
+  slides
+</template>
+
+<script>
+import Slides from './Slides'
+export default {
+  name: 'app',
+  components: { Slides }
+}
+</script>
+
+<style lang="scss">
+html, body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
